b557761c7b3761ff6356a62ec9df0a5f59a0784a
tharun200104 <tharunprof200104@gmail.com>
2026-09-19 01:49:49 +0530

    app first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/health":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"OK")
+            return
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"DropFisher DevOps Project")
+
+
+server = HTTPServer(("0.0.0.0", 8080), Handler)
+print("Server running on port 8080")
+server.serve_forever()
